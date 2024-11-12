edbdda25348a490375321f3a50b9d34778d078a8
Allan <allan.victor.dev@gmail.com>
2024-11-11 21:53:58 -0300

    Adicionar arquivos de projeto.

---
+++ Urna/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace Urna
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnCorrige = new System.Windows.Forms.Button();
+            this.btnBranco = new System.Windows.Forms.Button();
+            this.btnConfirma = new System.Windows.Forms.Button();
+            this.btn0 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.Controls.Add(this.btnCorrige);
+            this.panel1.Controls.Add(this.btnBranco);
+            this.panel1.Controls.Add(this.btnConfirma);
+            this.panel1.Controls.Add(this.btn0);
+            this.panel1.Controls.Add(this.btn9);
+            this.panel1.Controls.Add(this.btn8);
+            this.panel1.Controls.Add(this.btn7);
+            this.panel1.Controls.Add(this.btn6);
+            this.panel1.Controls.Add(this.btn5);
+            this.panel1.Controls.Add(this.btn4);
+            this.panel1.Controls.Add(this.btn3);
+            this.panel1.Controls.Add(this.btn2);
+            this.panel1.Controls.Add(this.btn1);
+            this.panel1.Location = new System.Drawing.Point(628, 117);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(360, 398);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnCorrige
+            // 
+            this.btnCorrige.BackgroundImage = global::Urna.Properties.Resources.corrige;
+            this.btnCorrige.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnCorrige.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnCorrige.Location = new System.Drawing.Point(130, 305);
+            this.btnCorrige.Name = "btnCorrige";
+            this.btnCorrige.Size = new System.Drawing.Size(98, 50);
+            this.btnCorrige.TabIndex = 12;
+            this.btnCorrige.UseVisualStyleBackColor = true;
+            // 
+            // btnBranco
+            // 
+            this.btnBranco.BackgroundImage = global::Urna.Properties.Resources.branco;
+            this.btnBranco.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnBranco.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnBranco.Location = new System.Drawing.Point(26, 305);
+            this.btnBranco.Name = "btnBranco";
+            this.btnBranco.Size = new System.Drawing.Size(98, 50);
+            this.btnBranco.TabIndex = 11;
+            this.btnBranco.UseVisualStyleBackColor = true;
+            // 
+            // btnConfirma
+            // 
+            this.btnConfirma.BackgroundImage = global::Urna.Properties.Resources.confirma;
+            this.btnConfirma.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnConfirma.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnConfirma.Location = new System.Drawing.Point(234, 246);
+            this.btnConfirma.Name = "btnConfirma";
+            this.btnConfirma.Size = new System.Drawing.Size(95, 109);
+            this.btnConfirma.TabIndex = 10;
+            this.btnConfirma.UseVisualStyleBackColor = true;
+            // 
+            // btn0
+            // 
+            this.btn0.BackgroundImage = global::Urna.Properties.Resources._0;
+            this.btn0.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btn0.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn0.Location = new System.Drawing.Point(142, 227);
+            this.btn0.Name = "btn0";
+            this.btn0.Size = new System.Drawing.Size(73, 50);
+            this.btn0.TabIndex = 9;
+            this.btn0.UseVisualStyleBackColor = true;
+            this.btn0.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // btn9
+            // 
+            this.btn9.BackgroundImage = global::Urna.Properties.Resources._9;
+            this.btn9.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btn9.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn9.Location = new System.Drawing.Point(236, 171);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(73, 50);
+            this.btn9.TabIndex = 8;
+            this.btn9.UseVisualStyleBackColor = true;
+            // 
+            // btn8
+            // 
+            this.btn8.BackgroundImage = global::Urna.Properties.Resources._8;
+            this.btn8.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btn8.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn8.Location = new System.Drawing.Point(142, 171);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(73, 50);
+            this.btn8.TabIndex = 7;
+            this.btn8.UseVisualStyleBackColor = true;
+            // 
+            // btn7
+            // 
+            this.btn7.BackgroundImage = global::Urna.Properties.Resources._7;
+            this.btn7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btn7.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn7.Location = new System.Drawing.Point(51, 171);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(73, 50);
+            this.btn7.TabIndex = 6;
+            this.btn7.UseVisualStyleBackColor = true;
+            // 
+            // btn6
+            // 
+            this.btn6.BackgroundImage = global::Urna.Properties.Resources._6;
+            this.btn6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btn6.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn6.Location = new System.Drawing.Point(236, 115);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(73, 50);
+            this.btn6.TabIndex = 5;
+            this.btn6.UseVisualStyleBackColor = true;
+            // 
+            // btn5
+            // 
+            this.btn5.BackgroundImage = global::Urna.Properties.Resources._5;
+            this.btn5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btn5.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn5.Location = new System.Drawing.Point(142, 115);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(73, 50);
+            this.btn5.TabIndex = 4;
+            this.btn5.UseVisualStyleBackColor = true;
+            // 
+            // btn4
+            // 
+            this.btn4.BackgroundImage = global::Urna.Properties.Resources._4;
+            this.btn4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btn4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn4.Location = new System.Drawing.Point(51, 115);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(73, 50);
+            this.btn4.TabIndex = 3;
+            this.btn4.UseVisualStyleBackColor = true;
+            // 
+            // btn3
+            // 
+            this.btn3.BackgroundImage = global::Urna.Properties.Resources._3;
+            this.btn3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btn3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn3.Location = new System.Drawing.Point(236, 59);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(73, 50);
+            this.btn3.TabIndex = 2;
+            this.btn3.UseVisualStyleBackColor = true;
+            // 
+            // btn2
+            // 
+            this.btn2.BackgroundImage = global::Urna.Properties.Resources._2;
+            this.btn2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btn2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn2.Location = new System.Drawing.Point(142, 59);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(73, 50);
+            this.btn2.TabIndex = 1;
+            this.btn2.UseVisualStyleBackColor = true;
+            // 
+            // btn1
+            // 
+            this.btn1.BackgroundImage = global::Urna.Properties.Resources._1;
+            this.btn1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btn1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn1.Location = new System.Drawing.Point(51, 59);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(73, 50);
+            this.btn1.TabIndex = 0;
+            this.btn1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1039, 583);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btn0;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btnCorrige;
+        private System.Windows.Forms.Button btnBranco;
+        private System.Windows.Forms.Button btnConfirma;
+    }
+}
+
